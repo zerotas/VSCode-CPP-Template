6979ac7920c6f34aba6c2ad102b71c9743959761
MangoChow <xianpengliu@hotmail.com>
2018-10-09 18:13:12 +0800

    规范task的名称; 整理main.cpp中的测试代码

--- Template/src/main.cpp
+++ Template/src/main.cpp
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <type_traits>
 #include <iostream>
-#include <string>
 #include "include1.h"
 #include "include2.h"
 
 int main()
 {
-    //printf("3 + 4 = %d\n", add(3, 4));
-    printf("Hello World!:%ld\n", __cplusplus);
+    std::cout << "C++ Version: " << __cplusplus << std::endl;
+    
+    std::cout << "3 + 4 = " << add(3, 4) << std::endl;
+    std::cout << "3 - 4 = " << sub(3, 4) << std::endl;
+
     system("pause");
 
     return 0;
